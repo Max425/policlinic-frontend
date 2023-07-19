432bfd84f7dd4104769eb9581357b8277d4a92bb
ksenia0807 <kadirovak08@mail.ru>
2023-07-19 18:03:37 +0300

    [DS-14]-add-save

--- policlinic/src/app/components/visitor/visitor.component.ts
+++ policlinic/src/app/components/visitor/visitor.component.ts
@@ -13,7 +13,19 @@ export class VisitorComponent implements OnInit {
   @Input() visitor: IVisitor
   details = false
   redactor = false
+  check = false
   records: IRecord[] = data
+
+  FirstName: string;
+  LastName: string;
+  FatherName: string;
+  City: string;
+  Gender: string;
+  BirthDate: string;
+  Nationality: string;
+  PassportSeries: number;
+  PassportNumber: number;
+  DateIssue: string;
 
   constructor(public dbService: InteractionsWithDbApiService) {}
   ngOnInit(): void { // запрос делать если details = true а не при инициализации
@@ -24,6 +36,16 @@ export class VisitorComponent implements OnInit {
 
   save(): void {
     console.log("save")
-    this.dbService.interactionsWithDbCreateVisitorPost$Response()
+    this.dbService.interactionsWithDbCreateVisitorPost$Response({
+      firstName: this.FirstName,
+      lastName: this.LastName,
+      fatherName: this.FatherName,
+      city: this.City,
+      gender: this.Gender,
+      birthDate: this.BirthDate,
+      nationality: this.Nationality,
+      passportSeries: this.PassportSeries,
+      passportNumber: this.PassportNumber,
+      dateIssue: this.DateIssue})
   }
 }
