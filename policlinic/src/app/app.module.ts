04ac54a4d7f7825c6a6115d7d32ff7921c0b6106
Алина Аитова <aitova-2003@mail.ru>
2023-07-21 20:00:01 +0400

    [DS-28]-create-personal-account

--- policlinic/src/app/app.module.ts
+++ policlinic/src/app/app.module.ts
@@ -33,6 +33,7 @@ import { RecordComponent } from './components/record/record.component';
 import { NotificationsComponent } from './pages/notifications/notifications.component';
 import { ConflictVisitorComponent } from './components/conflict-visitor/conflict-visitor.component';
 import { HttpClientModule } from '@angular/common/http';
+import { PersonalaccountComponent } from './components/personalaccount/personalaccount.component';
 
 @NgModule({
   declarations: [
@@ -45,7 +46,8 @@ import { HttpClientModule } from '@angular/common/http';
     FilterVisitorsPipe,
     RecordComponent,
     NotificationsComponent,
-    ConflictVisitorComponent
+    ConflictVisitorComponent,
+    PersonalaccountComponent
   ],
   imports: [
     HttpClientModule,
---
+++ policlinic/src/app/components/personalaccount/personalaccount.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-personalaccount',
+  templateUrl: './personalaccount.component.html',
+  styleUrls: ['./personalaccount.component.css']
+})
+export class PersonalaccountComponent {
+
+}
